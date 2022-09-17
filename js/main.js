a0e6755b54ce8a2d2f04bcc11f47cec5f72dec78
mazen-mahfouz <75842519+mazen-mahfouz@users.noreply.github.com>
2022-09-18 00:18:11 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-        let filters = {
-    'all': function (todos) {
-        return todos
-    },
-    'active': function (todos) {
-        return todos.filter(function(todo){
-            return ! todo.complet
-        })
-    },
-    'complet': function (todos) {
-        return todos.filter(function(todo){
-            return todo.complet
-        })
-    }
-}
+//         let filters = {
+//     'all': function (todos) {
+//         return todos
+//     },
+//     'active': function (todos) {
+//         return todos.filter(function(todo){
+//             return ! todo.complet
+//         })
+//     },
+//     'complet': function (todos) {
+//         return todos.filter(function(todo){
+//             return todo.complet
+//         })
+//     }
+// }
 
 
 var app = new Vue({
@@ -45,21 +45,21 @@ var app = new Vue({
       'remove_item': function(x){
         this.todo.splice(x,1)
       },
-      'complet_app': function(){
-        if(filters['active'] (this.todo).length == 0){
-            return filters['complet'](this.todo).filter(function(el){
-                el.complet = !el.complet;
-            })
-        }else{
-            return filters['all'](this.todo).filter(function(el){
-                if(el.complet == false){
-                    el.complet = !el.complet;
-                }
-            })
-        }
-      },
+//       'complet_app': function(){
+//         if(filters['active'] (this.todo).length == 0){
+//             return filters['complet'](this.todo).filter(function(el){
+//                 el.complet = !el.complet;
+//             })
+//         }else{
+//             return filters['all'](this.todo).filter(function(el){
+//                 if(el.complet == false){
+//                     el.complet = !el.complet;
+//                 }
+//             })
+//         }
+//       },
       'remove_complet': function () {
-        this.todo = filters['active'] (this.todo)
+//         this.todo = filters['active'] (this.todo)
       }
     },
     computed: {
@@ -69,16 +69,16 @@ var app = new Vue({
         },
        'filters_active': function(){
             localStorage.setItem('item', JSON.stringify(this.todo));
-            return filters[this.visibility](this.todo)
+//             return filters[this.visibility](this.todo)
            
        },
-       'count_active': function(){
-        if(filters['active'] (this.todo).length == 0 && filters['all'] (this.todo).length > 0){
-            this.alldone = true;
-        }else{
-            this.alldone = false;
-        }
-           return filters['active'] (this.todo).length;
-       },
+//        'count_active': function(){
+//         if(filters['active'] (this.todo).length == 0 && filters['all'] (this.todo).length > 0){
+//             this.alldone = true;
+//         }else{
+//             this.alldone = false;
+//         }
+//            return filters['active'] (this.todo).length;
+//        },
     },
 })
